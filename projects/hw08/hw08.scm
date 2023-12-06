6922991ed1af776c228d96c7aecaf4ec3a50b696
drdracool <jadexing@foxmail.com>
2023-12-06 20:55:15 +0900

    hw08 completed

---
+++ projects/hw08/hw08.scm
@@ -0,0 +1,20 @@
+(define (square n) (* n n))
+
+(define (pow base exp) 
+  (if (= exp 0)
+      1
+      (if (even? exp)
+          (pow (* base base) (/ exp 2))
+          (* base (pow (* base base) (/ (- exp 1) 2))))))
+
+(define (repeatedly-cube n x)
+  (if (zero? n)
+      x
+      (let ((y (repeatedly-cube (- n 1) x)))
+        (* y y y))))
+
+(define (cddr s) (cdr (cdr s)))
+
+(define (cadr s) (car (cdr s)))
+
+(define (caddr s) (car (cddr s)))
