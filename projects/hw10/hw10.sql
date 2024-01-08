97faaf7edecb4c23030e6182bea11f32656df35f
drdracool <jadexing@foxmail.com>
2024-01-08 15:19:38 -0800

    hw10 completed

---
+++ projects/hw10/hw10.sql
@@ -0,0 +1,50 @@
+CREATE TABLE parents AS
+  SELECT "abraham" AS parent, "barack" AS child UNION
+  SELECT "abraham"          , "clinton"         UNION
+  SELECT "delano"           , "herbert"         UNION
+  SELECT "fillmore"         , "abraham"         UNION
+  SELECT "fillmore"         , "delano"          UNION
+  SELECT "fillmore"         , "grover"          UNION
+  SELECT "eisenhower"       , "fillmore";
+
+CREATE TABLE dogs AS
+  SELECT "abraham" AS name, "long" AS fur, 26 AS height UNION
+  SELECT "barack"         , "short"      , 52           UNION
+  SELECT "clinton"        , "long"       , 47           UNION
+  SELECT "delano"         , "long"       , 46           UNION
+  SELECT "eisenhower"     , "short"      , 35           UNION
+  SELECT "fillmore"       , "curly"      , 32           UNION
+  SELECT "grover"         , "short"      , 28           UNION
+  SELECT "herbert"        , "curly"      , 31;
+
+CREATE TABLE sizes AS
+  SELECT "toy" AS size, 24 AS min, 28 AS max UNION
+  SELECT "mini"       , 28       , 35        UNION
+  SELECT "medium"     , 35       , 45        UNION
+  SELECT "standard"   , 45       , 60;
+
+
+-- All dogs with parents ordered by decreasing height of their parent
+CREATE TABLE by_parent_height AS
+  SELECT a.child FROM parents AS a, dogs AS b WHERE a.parent = b.name ORDER BY b.height DESC;
+
+
+-- The size of each dog
+CREATE TABLE size_of_dogs AS
+  SELECT name, size FROM dogs, sizes WHERE height > min AND height <= max;
+
+
+-- Filling out this helper table is optional
+CREATE TABLE siblings AS
+  SELECT a.child AS sib1, b.child AS sib2 FROM parents AS a, parents AS b WHERE a.child < b.child AND a.parent = b.parent;
+
+-- Sentences about siblings that are the same size
+CREATE TABLE sentences AS
+  SELECT "The two siblings, " || sib1 || " plus " || sib2 ||  " have the same size: " || a.size FROM siblings, size_of_dogs AS a, size_of_dogs AS b WHERE sib1 = a.name AND sib2 = b.name AND a.size = b.size;
+
+
+
+-- Height range for each fur type where all of the heights differ by no more than 30% from the average height
+CREATE TABLE low_variance AS
+  SELECT fur, MAX(height) - MIN(height) FROM dogs GROUP BY fur HAVING MAX(height) <= 1.3 * AVG(height) AND MIN(height) >= 0.7 * AVG(height);
+
